5152d815b3b925a8858864229af5b7e1a3950c10
Ivan Furman <vanyafurman13@gmail.com>
2024-05-13 20:12:07 +0300

    swagger start

--- src/server.js
+++ src/server.js
@@ -3,8 +3,12 @@ const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerSpec.js'); 
+
 const router = require("./routes")
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json())
 app.use("/api", router)
 
