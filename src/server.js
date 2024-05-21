78a51d33b61ff3141241823e1f3c67c3fff17c19
Ivan Furman <vanyafurman13@gmail.com>
2024-05-21 21:10:44 +0300

    downloading dependencies

--- src/server.js
+++ src/server.js
@@ -5,15 +5,13 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const PORT = process.env.PORT
-Sentry.setupExpressErrorHandler(app);
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('../swaggerSpec.js'); 
 
 const router = require("./routes")
 
-// app.use(Sentry.Handlers.requestHandler());
-// app.use(Sentry.Handlers.tracingHandler());
-// app.use(Sentry.Handlers.errorHandler());
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json())
@@ -23,6 +21,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
 
+Sentry.setupExpressErrorHandler(app);
 app.listen(PORT, ()=>{
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 })
